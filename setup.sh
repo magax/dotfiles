28f750ec25ce13b9d1e3195b7e3d7829fead0ba1
magax <max.janvier@epitech.eu>
2023-07-24 00:15:49 +0200

    :wrench: Add TMUX plugin manager to setup.sh
    Remove individual plugins git clones

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-### ZSH PLUGINS
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# TMUX plugin manager
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
