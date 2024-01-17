2dd08ddc6f71e3b8d76d37f48de9b33d1cf0b6fc
magax <max.janvier@epitech.eu>
2024-01-17 18:41:15 +0100

    :wrench: Update setup file

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
-# TMUX plugin manager
+# DOTFILES
+git clone https://github.com/magax/dotfiles.git
 
+# TMUX plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# HOMEBREW
+cd ~ && mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+brew bundle --file ~/dotfiles/Brewfile
