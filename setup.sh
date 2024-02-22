584bb80eee17beeb2cc84ec5d71e2e37fede7e91
magax <max.janvier@epitech.eu>
2024-02-22 10:06:37 +0100

    :wrench: Update setup file
    - Dotfiles folder always cloned to Home directory
    - Oh My Zsh installation added
    - Config files' symlinks added to Home directory

--- setup.sh
+++ setup.sh
@@ -1,9 +1,21 @@
-# DOTFILES
-git clone https://github.com/magax/dotfiles.git
+# Get dotfiles
+git clone https://github.com/magax/dotfiles.git ~/dotfiles
 
-# TMUX plugin manager
+# Get TMUX plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# HOMEBREW
-cd ~ && mkdir homebrew && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew bundle --file ~/dotfiles/Brewfile
+# Get Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install brew packages from Brewfile
+brew bundle --file ~/dotfiles/.Brewfile
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Create symlinks to Home
+ln -s ~/dotfiles/.Brewfile ~
+ln -s ~/dotfiles/.gitconfig ~
+ln -s ~/dotfiles/.tmux.conf ~
+ln -s -f ~/dotfiles/.zshrc ~
+ln -s ~/dotfiles/nvim ~
