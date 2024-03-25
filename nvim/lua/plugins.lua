78c74b6f591985ab92f7641c1fedd87363b513f7
magax <max.janvier@epitech.eu>
2024-03-25 14:43:32 +0100

    :package: Add  package
    Also format lua files

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,93 +1,104 @@
 -- Check packer installation
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd [[packadd packer.nvim]]
+  packer_bootstrap =
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+  vim.cmd([[packadd packer.nvim]])
 end
 
 local opts = { silent = true, noremap = true }
 
-return require('packer').startup(function(use)
-
+return require("packer").startup(function(use)
   -- Packer can manage itself`
-  use 'wbthomason/packer.nvim' 
+  use("wbthomason/packer.nvim")
 
   -- Configurations for Nvim LSP
-  use {
+  use({
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-    end
-  }
+    end,
+  })
 
-  use {
-    'neovim/nvim-lspconfig',
+  use("williamboman/mason-lspconfig.nvim")
+
+  use({
+    "neovim/nvim-lspconfig",
     config = function()
-      require('config/lsp_rc')
-    end
-  }
+      require("config/lsp_rc")
+    end,
+  })
 
-  use "williamboman/mason-lspconfig.nvim"
+  use({
+    "nvimtools/none-ls.nvim",
+    config = function()
+      require("config/none-ls_rc")
+    end,
+    requires = { "nvim-lua/plenary.nvim" },
+  })
 
   -- Color schemes
-  use {
-    'dracula/vim', as = 'dracula',
-    config = function() 
-      vim.cmd('colorscheme dracula')
-      vim.cmd(':highlight Normal ctermbg=black')
-    end
-  }
+  use({
+    "dracula/vim",
+    as = "dracula",
+    config = function()
+      vim.cmd("colorscheme dracula")
+      vim.cmd(":highlight Normal ctermbg=black")
+    end,
+  })
 
   -- Icons
-  use 'nvim-tree/nvim-web-devicons'
+  use("nvim-tree/nvim-web-devicons")
 
   -- Tree-sitter
-  use {
-    'nvim-treesitter/nvim-treesitter',
+  use({
+    "nvim-treesitter/nvim-treesitter",
     config = function()
-      require('config/treesitter_rc')
-    end
-  }
-  use 'nvim-treesitter/nvim-treesitter-context'
+      require("config/treesitter_rc")
+    end,
+  })
+  use("nvim-treesitter/nvim-treesitter-context")
 
   -- FZF Lua
-  use {
-    'ibhagwan/fzf-lua',
+  use({
+    "ibhagwan/fzf-lua",
     config = function()
-      require('config/fzf_rc')
-    end
-  }
+      require("config/fzf_rc")
+    end,
+  })
 
   -- Completion
-  use {
-    'hrsh7th/nvim-cmp',
+  use({
+    "hrsh7th/nvim-cmp",
     config = function()
-      require('config/cmp_rc')
-    end
-  } -- Engine
-  use { 'hrsh7th/cmp-nvim-lsp' } -- LSP completion
-  use { 'hrsh7th/cmp-buffer' } -- buffer completion
-  use { 'hrsh7th/cmp-path' } -- path completion
+      require("config/cmp_rc")
+    end,
+  }) -- Engine
+  use({ "hrsh7th/cmp-nvim-lsp" }) -- LSP completion
+  use({ "hrsh7th/cmp-buffer" }) -- buffer completion
+  use({ "hrsh7th/cmp-path" }) -- path completion
   -- snippet completion
-  use { 'L3MON4D3/LuaSnip' }
-  use { 'saadparwaiz1/cmp_luasnip' } 
+  use({ "L3MON4D3/LuaSnip" })
+  use({ "saadparwaiz1/cmp_luasnip" })
 
   -- Vim Airline
-  use 'vim-airline/vim-airline'
+  use("vim-airline/vim-airline")
 
   -- Comments
-  use 'preservim/nerdcommenter'
+  use("preservim/nerdcommenter")
 
   -- Autopairs
-  use {
-    'windwp/nvim-autopairs',
-    config = function() require('nvim-autopairs').setup {} end
-  }
+  use({
+    "windwp/nvim-autopairs",
+    config = function()
+      require("nvim-autopairs").setup({})
+    end,
+  })
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
-    require('packer').sync()
+    require("packer").sync()
   end
 end)
