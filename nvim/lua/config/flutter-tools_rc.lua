e4b502cd6f6a091e3c5d6017c601c7025a90171d
magax <max.janvier@epitech.eu>
2024-10-27 12:11:07 +0800

    🔧 [Neovim] Update keymaps

--- nvim/lua/config/flutter-tools_rc.lua
+++ nvim/lua/config/flutter-tools_rc.lua
@@ -14,10 +14,10 @@ require'flutter-tools'.setup {
 }
 
 local opts = { silent = true, noremap = true }
-vim.keymap.set('n', '<leader>fS', ':FlutterRun --dart-define=RUN_EMULATORS=true -d ipad<CR>', opts)
-vim.keymap.set('n', '<leader>fr', ':FlutterReload<CR>', opts)
-vim.keymap.set('n', '<leader>fR', ':FlutterRestart<CR>', opts)
-vim.keymap.set('n', '<leader>fd', ':FlutterOpenDevTools<CR>', opts)
-vim.keymap.set('n', '<leader>fc', ':FlutterLogClear<CR>', opts)
-vim.keymap.set('n', '<leader>fQ', ':FlutterQuit<CR>', opts)
+vim.keymap.set('n', '<space>fS', ':FlutterRun --dart-define=RUN_EMULATORS=true -d ipad<CR>', opts)
+vim.keymap.set('n', '<space>fr', ':FlutterReload<CR>', opts)
+vim.keymap.set('n', '<space>fR', ':FlutterRestart<CR>', opts)
+vim.keymap.set('n', '<space>fd', ':FlutterOpenDevTools<CR>', opts)
+vim.keymap.set('n', '<space>fc', ':FlutterLogClear<CR>', opts)
+vim.keymap.set('n', '<space>fQ', ':FlutterQuit<CR>', opts)
 
